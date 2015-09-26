b82442079f3cd8a09a4a47ca019d40602d6c599c
trycrmr <trycrmr@gmail.com>
2015-09-26 10:14:16 -0400

    added users to search migration. Now a user has many searches and a search belongs to a user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150925224128) do
+ActiveRecord::Schema.define(version: 20150926141145) do
 
   create_table "favorites", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -26,7 +26,10 @@ ActiveRecord::Schema.define(version: 20150925224128) do
   create_table "searches", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
+
+  add_index "searches", ["user_id"], name: "index_searches_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
