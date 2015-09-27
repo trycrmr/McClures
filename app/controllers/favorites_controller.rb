cafdb05d020d6b47c16ef4b345232c4505c5300a
trycrmr <trycrmr@gmail.com>
2015-09-26 20:58:58 -0400

    a user is able to see all their favorites from the index view of the favorites controller. It is also integrated into the rest of the application.
    The next step is to perform an initial push to heroku, then style the application, then refine the location search to resolve to a longitude and latitude as opposed to a zip code.

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,6 +2,18 @@ class FavoritesController < ApplicationController
 	before_action :authenticate_user!
 
   def index
+    @this_user_id = current_user.id
+    # @this_users_favorites = []
+    @this_users_favorite_politicians = []    
+    Favorite.find_each do |a_favorite|
+      # @this_users_favorites.push(a_favorite)
+      @this_politicians_bioguide_id = Politician.find_by(id: a_favorite.politician_id).bioguide_id
+      puts @this_politicians_bioguide_id
+      this_politician = API_Politician.get_array_of_politicians_from_SF_Congress_API_call("legislators", bioguide_id: @this_politicians_bioguide_id).pop
+      @this_users_favorite_politicians.push(this_politician)
+    end
+
+
   end
 
   def create
@@ -9,6 +21,7 @@ class FavoritesController < ApplicationController
   	puts @this_user_id
   	@this_politician = params.has_key?(:bioguide_id) ? params[:bioguide_id] : nil
   	@this_politician_id = @this_politician ? Politician.find_by(bioguide_id: @this_politician).id : nil
-  	@this_favorite = Favorite.create(user_id: @this_user_id, politician_id: @this_politician_id)
+  	@this_favorite = Favorite.find_or_create_by(user_id: @this_user_id, politician_id: @this_politician_id)
+    redirect_to favorites_index_path
   end
 end
--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,5 +1,6 @@
 class Favorite < ActiveRecord::Base
 
 	validates :user_id, presence: true	
-	validates :politician_id, presence: true			
+	validates :politician_id, presence: true
+	# validates :id, :uniqueness, true		
 end
