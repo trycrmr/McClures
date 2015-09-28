b8498266eb03dd3270c408374490dec2d265a52e
trycrmr <trycrmr@gmail.com>
2015-09-28 18:36:36 -0400

    Resolved an issue where any user's stash was showing all the politicians that have ever been favorited. A user's stash is supposed to show only the politicians they have added to their stash.

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,16 +3,16 @@ class FavoritesController < ApplicationController
 
   def index
     @this_user_id = current_user.id
-    # @this_users_favorites = []
-    @this_users_favorite_politicians = []    
+    @this_users_favorite_politicians = []
     Favorite.find_each do |a_favorite|
-      # @this_users_favorites.push(a_favorite)
-      @this_politicians_bioguide_id = Politician.find_by(id: a_favorite.politician_id).bioguide_id
-      puts @this_politicians_bioguide_id
-      this_politician = API_Politician.get_array_of_politicians_from_SF_Congress_API_call("legislators", bioguide_id: @this_politicians_bioguide_id).pop
-      @this_users_favorite_politicians.push(this_politician)
+      if a_favorite.user_id == @this_user_id
+        @this_politicians_bioguide_id = Politician.find_by(id: a_favorite.politician_id).bioguide_id
+        this_politician = API_Politician.get_array_of_politicians_from_SF_Congress_API_call("legislators", bioguide_id: @this_politicians_bioguide_id).pop
+        @this_users_favorite_politicians.push(this_politician)
+      else
+        #DO NOTHING
+      end
     end
-
 
   end
 
