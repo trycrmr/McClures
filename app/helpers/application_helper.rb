1d2b18cb32b8461192a53cb913838df1eaf1fec1
trycrmr <trycrmr@gmail.com>
2015-09-29 20:31:56 -0400

    Continued iterating over the new view of the search controller...
    added a helper to print the party name given the initial provided from Sunlight Foundation API
    set up the zip code text box to display the latest zip code searched

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+
+	def party_name_from_initial party_initial
+		case party_initial
+			when "R"
+			 "Republican"
+			when "D"
+			 "Democrat"
+			when "I"
+			 "Independent"
+			else
+			 "No party affiliation"
+		end		
+	end
+
+	def foo(test)
+		test
+	end
+
 end
